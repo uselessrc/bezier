db9225f8d0d16cc9ffa5820485c227c4200bdda5
boenfu <1997@boenfu.cn>
2020-12-15 19:05:56 +0800

    😪 Adjustment dependences

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,4 +1,4 @@
-import React, {CSSProperties, FC, Fragment, useEffect, useState} from 'react';
+import React, {FC, Fragment} from 'react';
 import ReactDOM from 'react-dom';
 
 // eslint-disable-next-line no-restricted-imports
@@ -33,8 +33,6 @@ const APP: FC = () => {
       ele,
       offset: {x, y},
     } = moving;
-
-    console.log(window.top);
 
     let {scrollTop, scrollLeft} = document.documentElement;
 
